9fc385d737929d5db96edf29384755723730fc71
marcos mora <abreu760@hotmail.com>
2026-05-16 23:01:53 +0000

    Cleanup deleted files from subfolder

--- turbobujias/functions/robots.txt.ts
+++
@@ -1,9 +0,0 @@
-export const onRequest: PagesFunction = async () => {
-  return new Response(
-    `User-agent: *
-Allow: /
-
-Sitemap: https://www.turbobujias.com/sitemap.xml`,
-    { headers: { 'Content-Type': 'text/plain' } }
-  );
-};
